fe9048f515076908332a8c99ca84fd658331699b
rameshn7 <53555028+rameshn7@users.noreply.github.com>
2025-05-10 18:39:48 +0530

    Create Xvideo.js

---
+++ Xvideo.js
@@ -0,0 +1,19 @@
+{
+  "url": "video.in",
+  "source": "video.in",
+  "author": "author ;",
+  "title": " title;",
+  "thumbnail": "thumbnail.jpg",
+  "duration": 370,
+  "medias": [
+    {
+      "url": "download link",
+      "quality": "HD",
+      "extension": "mp4",
+      "type": "video"
+    }
+  ],
+  "type": "single",
+  "error": false,
+  "time_end": 1351
+}
